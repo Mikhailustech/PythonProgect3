c9a16ffa4cb8d2f92df6c9211b2f9165d91d890a
mladygin <mladygin@usetech.ru>
2021-08-09 14:53:34 +0300

    Add real result in deposit

---
+++ main.py
@@ -0,0 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# Persents real on deposit
+def percents (x, y):
+     """What percentage of x is y"""
+     one_percent = x / 100
+     result = y / one_percent
+     print (str(y) + " is " + str(result) + "% of " + str(x))
+     print ("deposit = " +  str(x) + " in this moment " + str(y) + " result in procent " + str(result))
+percents (79.11, 88)
