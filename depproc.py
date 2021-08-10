35f2c57b450979cdfa2f0476f48dfd176d499f2e
mladygin <mladygin@usetech.ru>
2021-08-10 11:42:39 +0300

    release version

--- depproc.py
+++ depproc.py
@@ -3,14 +3,32 @@
 def percents(x, y):
      """What percentage of x is y"""
      one_percent = x / 100
-     result = y / one_percent
+     result = (y / one_percent) - 100
      return result
 
+
+def out_red(text):
+    print("\033[31m {}" .format(text))
+def out_yellow(text):
+    print("\033[33m {}" .format(text))
+def out_blue(text):
+    print("\033[34m {}" .format(text))
+
+
+def difine_color(result):
+     """Difine color for the result in percents"""
+     if result < 0:
+          color = out_red(result)
+     elif result > 0:
+          color = out_yellow(result)
+     else:
+          color = out_blue(result)
+     return color
 
 
 def print_percents(x,y):
      """Print What percentage of x is y"""
-     print("deposit = " +  str(x) + " in this moment " + str(y) + " result in procent " + str(percents(x, y)))
-    
+     print("deposit = " +  str(x) + " in this moment " + str(y) + " result in procents " + ""),(str(difine_color(percents(x, y))))
 
-print_percents(78,108)
+# Set the current values and see the result
+print_percents(79.11,119.54)
